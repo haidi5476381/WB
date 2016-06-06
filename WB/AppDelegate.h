fab3c97f881cc9a4860a22a9a5d67ee16e05b542
denghymf <denghymf@hehenian.com>
2016-06-06 10:17:00 +0800

    Initial Commit

---
+++ WB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WB
+//
+//  Created by 余海华 on 16/6/6.
+//  Copyright © 2016年 Haidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
