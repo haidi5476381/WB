79956ec83aa7e26d0e50d4902682ba04ff3252e5
denghymf <denghymf@hehenian.com>
2016-06-06 15:01:02 +0800

    first commit

---
+++ WB/Assistant/Constant.h
@@ -0,0 +1,13 @@
+//
+//  Constant.h
+//  WB
+//
+//  Created by 余海华 on 16/6/6.
+//  Copyright © 2016年 Haidi. All rights reserved.
+//
+
+#ifndef Constant_h
+#define Constant_h
+
+
+#endif /* Constant_h */
