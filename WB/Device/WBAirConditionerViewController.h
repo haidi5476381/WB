b7329b48820911985b0462c94fbe47e9c63f128f
haidi5476381@163.com <330416020@qq.com>
2016-07-15 17:47:50 +0800

    #.DVD页面

---
+++ WB/Device/WBAirConditionerViewController.h
@@ -0,0 +1,13 @@
+//
+//  WBAirConditionerViewController.h
+//  WB
+//
+//  Created by leihuiwu on 16/7/15.
+//  Copyright © 2016年 Haidi. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface WBAirConditionerViewController : BaseViewController
+
+@end
---
+++ WB/Device/WBDVDViewController.m
@@ -0,0 +1,39 @@
+//
+//  WBDVDViewController.m
+//  WB
+//
+//  Created by leihuiwu on 16/7/15.
+//  Copyright © 2016年 Haidi. All rights reserved.
+//
+
+#import "WBDVDViewController.h"
+
+@interface WBDVDViewController ()
+
+@end
+
+@implementation WBDVDViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    self.title = @"DVD";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
